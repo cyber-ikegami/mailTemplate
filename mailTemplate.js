7d34bad470ecac91158a8845ead5680892e5ca4c
cyber-ikegami <ikegami@cyberbeing.tech>
2021-08-20 16:26:39 +0900

    結果をテキストエリアに表示

--- mailTemplate.js
+++ mailTemplate.js
@@ -1,5 +1,5 @@
 function OnClickButton() {
-  document.getElementById("output").innerHTML
-  = document.getElementById("daigaku").value + "）" + document.getElementById("name").value + 
-  "様 <br> <br> お世話になっております。 <br> CSC）池上です。 <br> <br> 以上、よろしくお願いいたします。";
+  resultForm.resultTextArea.value=
+  document.getElementById("daigaku").value + "）" + document.getElementById("name").value + 
+  "様\n\nお世話になっております。\nCSC）池上です。\n\n以上、よろしくお願いいたします。";
 }
