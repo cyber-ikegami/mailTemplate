9bd4c4bdfc15551ebce6ce0306d14718a9c1457f
cyber-ikegami <ikegami@cyberbeing.tech>
2021-08-20 13:58:51 +0900

    .css、.jsの追加

---
+++ mailTemplate.js
@@ -0,0 +1,5 @@
+function OnClickButton() {
+  document.getElementById("output").innerHTML
+  = document.getElementById("daigaku").value + "）" + document.getElementById("name").value + 
+  "様 <br> <br> お世話になっております。 <br> CSC）池上です。 <br> <br> 以上、よろしくお願いいたします。";
+}
