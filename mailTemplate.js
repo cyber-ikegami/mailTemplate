b097c76ddd2312252734a1afd552fe297024851c
cyber-ikegami <ikegami@cyberbeing.tech>
2021-08-26 16:01:41 +0900

    決定ボタンの活性・非活性

--- mailTemplate.js
+++ mailTemplate.js
@@ -1,19 +1,30 @@
-function OnClickButton() {
+function InputCheck() {
 	var flag = 0;
+	var daigaku = document.getElementById("daigaku");
+	var name = document.getElementById("name");
 
-	if(document.getElementById("daigaku").value== ""){
+	if(daigaku.value== ""){
 		flag = 1;
 	}
-	else if(document.getElementById("name").value == ""){
+	else if(name.value == ""){
 		flag = 1;
 	}
 
 	if(flag){
-		window.alert('必須項目に未入力がありました'); 
-	}
-	else{
+		document.getElementById('kettei').disabled=true;
+	} else {
+	   	document.getElementById('kettei').disabled=false;
+	}	
+}
+
+function Reset(){
+	document.getElementById("daigaku").value = "";
+	document.getElementById("name").value = "";
+	InputCheck();
+}
+
+function OnClickButton() {
     resultForm.resultTextArea.value=
     document.getElementById("daigaku").value + "）" + document.getElementById("name").value + 
     "様\n\nお世話になっております。\nCSC）池上です。\n\n以上、よろしくお願いいたします。";
-	}
 }
