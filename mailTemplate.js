1d6c45b91fcd5aaab96a2dbbda00dbfccd4ae076
cyber-ikegami <ikegami@cyberbeing.tech>
2021-08-30 17:35:17 +0900

    プログラムの修正

--- mailTemplate.js
+++ mailTemplate.js
@@ -1,23 +1,28 @@
-// テンプレートタイプのチェック(空白/受付/パスワード)
+// テンプレートタイプのチェック(空白/受付/パスワード通知)
 function checkTemplate(){
 	const template = document.getElementById("template");
 	const str = template.options[template.selectedIndex].value;
-	if(str == "null"){
-		numberItem.hidden = true;
-		document.getElementById("number").value = "";
-		passwordItem.hidden = true;
-		document.getElementById("password").value = "";
-		inputRequiredCheck();
-	} else if(str == "reception") {
-		numberItem.hidden = false;
-		document.getElementById("password").value = "";
-		passwordItem.hidden = true;
-		inputRequiredCheck();
-	} else if(str == "password") {
-		passwordItem.hidden = false;
-		document.getElementById("number").value = "";
-		numberItem.hidden = true;
-		inputRequiredCheck();
+
+	switch(str){
+		case "null":
+			numberItem.hidden = true;
+			passwordItem.hidden = true;
+			document.getElementById("number").value = "";
+			document.getElementById("password").value = "";
+			inputRequiredCheck();
+			break;
+		case "reception":
+			numberItem.hidden = false;
+			passwordItem.hidden = true;
+			document.getElementById("password").value = "";
+			inputRequiredCheck();
+			break;
+		case "password":
+			passwordItem.hidden = false;
+			numberItem.hidden = true;
+			document.getElementById("number").value = "";
+			inputRequiredCheck();
+			break;
 	}
 }
 
@@ -30,9 +35,9 @@ function inputRequiredCheck() {
 	const determination = document.getElementById("determination");
 	let checkFormList = [university, name];
 	
-	if(numberItem.hidden == false){
+	if(!numberItem.hidden){
 		checkFormList.push(number);
-	} else if(passwordItem.hidden == false) {
+	} else if(!passwordItem.hidden) {
 		checkFormList.push(password);
 	}
 	
@@ -51,23 +56,26 @@ function resetForm(){
 	inputRequiredCheck();
 }
 
-// 出力処理
-function outputResult() {
-	const university = document.getElementById("university").value;
-	const name = document.getElementById("name").value;
+// 値の取得(案件管理番号)　memo：getPasswordValueとまとめる
+function getNumberValue(){
 	const number = document.getElementById("number").value;
-	const password = document.getElementById("password").value;
 	let numberValue = "";
-	let passwordValue = "";
 	
-	if(numberItem.hidden == false){
+    if(!numberItem.hidden){
 		numberValue = 
 		`
 		本件について、案件管理番号：${number}で受け付けました。
 		` .replace(/[ \t\r]+/g, "");
 	}
+	return numberValue
+}
 
-	if(passwordItem.hidden == false){
+// 値の取得(パスワード)
+function getPasswordValue(){
+	const password = document.getElementById("password").value;
+	let passwordValue = "";
+
+	if(!passwordItem.hidden){
 		passwordValue = 
 		`
 		先程の添付ファイルのパスワードは、以下になります。
@@ -76,6 +84,16 @@ function outputResult() {
 		------------------------------
 		` .replace(/[ \t\r]+/g, "");
 	}
+	return passwordValue;
+}
+
+// 出力処理
+function outputResult() {
+	const university = document.getElementById("university").value;
+	const name = document.getElementById("name").value;
+	let numberValue = getNumberValue();
+	let passwordValue = getPasswordValue();
+
     resultForm.resultTextArea.value=
     `${university})${name}様
 
