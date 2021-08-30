15f2c017d5fdc6209e85a53df7f580871c55adce
cyber-ikegami <ikegami@cyberbeing.tech>
2021-08-30 14:01:50 +0900

    入力必須チェックの修正

--- mailTemplate.js
+++ mailTemplate.js
@@ -14,20 +14,17 @@ function checkTemplate(){
 
 // 入力必須チェック
 function inputRequiredCheck() {
-	let flag = 0;
-	const university = document.getElementById("university");
-	const name = document.getElementById("name");
+	const university = document.getElementById("university").value;
+	const name = document.getElementById("name").value;
+	const number = document.getElementById("number").value;
 	const determination = document.getElementById("determination");
-	const number = document.getElementById("number");
-
-	if(university.value == ""){
-		flag = 1;
-	} else if(name.value == ""){
-		flag = 1;
-	} else if(numberItem.hidden == false && number.value == ""){
-		flag = 1;
+	let checkFormList = [university, name];
+	
+	if(numberItem.hidden == false){
+		checkFormList.push(number);
 	}
-	determination.disabled=flag;
+	
+	determination.disabled = (checkFormList.includes(""));
 }
 
 // リセット処理
